da8d566c393705d1e476ade29dc7d5c4fb869395
Scott Yargeau <scottyargeau@gmail.com>
2017-12-27 14:23:41 -0800

    FT now testing display of ten most recent articles

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,5 @@ def home_page(request):
     """
     Renders the view for our home page.
     """
-    article = Article.get_articles(page=1)
-    return render(request, 'home.html', {'article': article})
+    articles = Article.get_articles(page=1)
+    return render(request, 'home.html', {'article': articles})
--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -1,9 +1,12 @@
 """
-End to end functional tests that test the integration of the home page from the users viewpoint.
+End to end functional tests that test the integration of the home page
+from the users viewpoint.
 """
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
+
+from blog.models import Article
 
 
 class BasicHomePageTest(LiveServerTestCase):
@@ -11,12 +14,26 @@ class BasicHomePageTest(LiveServerTestCase):
     Tests that we can get a basic home page up and running.
     """
 
+    @classmethod
+    def setUpClass(cls):
+        """
+        Initiate the whole testing suite by adding articles to the
+        testing database.
+        """
+        super().setUpClass()
+        num_articles = 11
+        for article in range(1, (num_articles + 1)):
+            Article.objects.create(
+                title=f'Test Article {article}',
+                body=f'Test body content {article}',
+                published=True
+            )
+
     def setUp(self):
         """
         Initiate every test by setting up the browser.
         """
         self.browser = webdriver.Firefox()
-        self.recent_post_title = 'Recent Post'
 
     def tearDown(self):
         """
@@ -32,8 +49,12 @@ class BasicHomePageTest(LiveServerTestCase):
         # Bob logs into Moto Now Blog, the best site ever! He notices it's title.
         desired_title = 'Moto Now Blog'
         self.assertEqual(self.browser.title, desired_title)
-        # Bob sees the most recent post on the home page.
-        recent_post = self.browser.find_element_by_class_name('recent-post')
-        self.assertIn(self.recent_post_title, recent_post.text)
+        # Bob sees the title of the 10 most recent posts on the home page.
+        # Note: in our test case, these articles are given the numbered titles 1-11,
+        # so the most recent ones will be 2-11.
+        page_text = self.browser.find_element_by_tag_name('body').text
+        for article in range(2, 12):
+            self.assertIn(f'Test Article {article}', page_text)
+        self.assertNotIn('Test Article 1', page_text)
         # Continue testing...
         self.fail('Finish all tests first!')
